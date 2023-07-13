34c6db0ea15c83b7ff040ff85ec865ee88fd0fc7
Argens <argens1203@gmail.com>
2023-07-13 23:21:04 +0800

    feat: static floating

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { computePosition } from "@floating-ui/react";
+import "./App.css";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const buttonRef = useRef(null);
+  const tooltipRef = useRef(null);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  useEffect(() => {
+    computePosition(buttonRef.current, tooltipRef.current).then((pos) =>
+      setPos(pos)
+    );
+  });
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <button ref={buttonRef} id="button" aria-describedby="tooltip">
+          My button
+        </button>
+        <div
+          ref={tooltipRef}
+          style={{ left: pos.x, top: pos.y }}
+          id="tooltip"
+          role="tooltip"
         >
-          Learn React
-        </a>
+          My tooltip
+        </div>
       </header>
     </div>
   );
