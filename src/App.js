83611e58814e3ae26e798c5756ba3f9e4cb7377a
Argens <argens1203@gmail.com>
2023-07-14 21:49:54 +0800

    feat(middleware): arrow, offset, flip, shift

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import { computePosition } from "@floating-ui/react";
+import {
+  computePosition,
+  flip,
+  shift,
+  offset,
+  arrow,
+} from "@floating-ui/react";
 import "./App.css";
 import { useEffect, useRef, useState } from "react";
 
 function App() {
   const buttonRef = useRef(null);
   const tooltipRef = useRef(null);
+  const arrowRef = useRef(null);
   const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [arrowPos, setArrowPos] = useState({ x: 0, y: 0 });
+  const [placement, setPlacement] = useState("bottom");
+
   useEffect(() => {
-    computePosition(buttonRef.current, tooltipRef.current).then((pos) =>
-      setPos(pos)
-    );
+    computePosition(buttonRef.current, tooltipRef.current, {
+      middleware: [
+        offset(5),
+        flip(),
+        shift({ padding: 10 }),
+        arrow({ element: arrowRef.current }),
+      ],
+    }).then(({ x, y, placement, middlewareData }) => {
+      setPos({ x, y });
+      setPlacement(placement);
+
+      const { x: arrowX, y: arrowY } = middlewareData.arrow || {};
+      setArrowPos({ x: arrowX, y: arrowY });
+    });
   });
+
+  const staticSide = {
+    top: "bottom",
+    right: "left",
+    bottom: "top",
+    left: "right",
+  }[placement.split("-")[0]];
 
   return (
     <div className="App">
       <header className="App-header">
-        <button ref={buttonRef} id="button" aria-describedby="tooltip">
+        <button
+          ref={buttonRef}
+          id="button"
+          aria-describedby="tooltip"
+          style={{ margin: 5 }}
+        >
           My button
         </button>
         <div
@@ -25,6 +58,15 @@ function App() {
           role="tooltip"
         >
           My tooltip
+          <div
+            id="arrow"
+            ref={arrowRef}
+            style={{
+              left: arrowPos.x,
+              top: arrowPos.y,
+              [staticSide]: -4,
+            }}
+          />
         </div>
       </header>
     </div>
