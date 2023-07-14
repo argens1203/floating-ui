054f4025a3630807a64feb7b4acab3d28d7fba38
Argens <argens1203@gmail.com>
2023-07-15 00:06:14 +0800

    feat: interactive (onHover, onFocus)

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ import "./App.css";
 import { useEffect, useRef, useState } from "react";
 
 function App() {
+  console.log("rerendering");
   const buttonRef = useRef(null);
   const tooltipRef = useRef(null);
   const arrowRef = useRef(null);
+
+  const [value, setValue] = useState("");
+  const [show, setShow] = useState(false);
+
   const [pos, setPos] = useState({ x: 0, y: 0 });
   const [arrowPos, setArrowPos] = useState({ x: 0, y: 0 });
-  const [placement, setPlacement] = useState("bottom");
+  const [staticSide, setStaticSide] = useState("bottom");
 
   useEffect(() => {
     computePosition(buttonRef.current, tooltipRef.current, {
@@ -26,49 +31,65 @@ function App() {
       ],
     }).then(({ x, y, placement, middlewareData }) => {
       setPos({ x, y });
-      setPlacement(placement);
+
+      const staticSide = {
+        top: "bottom",
+        right: "left",
+        bottom: "top",
+        left: "right",
+      }[placement.split("-")[0]];
+      setStaticSide(staticSide);
 
       const { x: arrowX, y: arrowY } = middlewareData.arrow || {};
       setArrowPos({ x: arrowX, y: arrowY });
     });
-  });
+  }, []);
 
-  const staticSide = {
-    top: "bottom",
-    right: "left",
-    bottom: "top",
-    left: "right",
-  }[placement.split("-")[0]];
+  const showTooltip = () => {
+    console.log("showTooltip");
+    setShow(true);
+  };
+  const hideTooltip = () => {
+    console.log("hideTooltip");
+    setShow(false);
+  };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <button
-          ref={buttonRef}
-          id="button"
-          aria-describedby="tooltip"
-          style={{ margin: 5 }}
-        >
-          My button
-        </button>
+      {/* <header className="App-header"></header> */}
+      {/* <body> */}
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button
+        tabIndex={0}
+        ref={buttonRef}
+        id="button"
+        aria-describedby="tooltip"
+        style={{ margin: 5 }}
+        onMouseEnter={showTooltip}
+        onMouseLeave={hideTooltip}
+        onFocus={showTooltip}
+        onBlur={hideTooltip}
+      >
+        My button
+      </button>
+      <div
+        ref={tooltipRef}
+        style={{ left: pos.x, top: pos.y, display: show ? "block" : "none" }}
+        id="tooltip"
+        role="tooltip"
+      >
+        My tooltip
         <div
-          ref={tooltipRef}
-          style={{ left: pos.x, top: pos.y }}
-          id="tooltip"
-          role="tooltip"
-        >
-          My tooltip
-          <div
-            id="arrow"
-            ref={arrowRef}
-            style={{
-              left: arrowPos.x,
-              top: arrowPos.y,
-              [staticSide]: -4,
-            }}
-          />
-        </div>
-      </header>
+          id="arrow"
+          ref={arrowRef}
+          style={{
+            left: arrowPos.x,
+            top: arrowPos.y,
+            [staticSide]: -4,
+          }}
+        />
+      </div>
+      {/* </body> */}
     </div>
   );
 }
